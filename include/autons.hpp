ea1f02c079f37768b77fccec60e25d4bfa92cb58
Nicholas Jors <tnjors@gmail.com>
2025-10-04 17:17:56 -0500

    initial

---
+++ include/autons.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+void default_constants();
+
+void drive_example();
+void turn_example();
+void drive_and_turn();
+void wait_until_change_speed();
+void swing_example();
+void motion_chaining();
+void combining_movements();
+void interfered_example();
+void odom_drive_example();
+void odom_pure_pursuit_example();
+void odom_pure_pursuit_wait_until_example();
+void odom_boomerang_example();
+void odom_boomerang_injected_pure_pursuit_example();
+void measure_offsets();
+
+void drive_v1();
+
---
+++ include/subsystems.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::MotorGroup intake({-6, -5});
+inline pros::MotorGroup intakeTop({4});
